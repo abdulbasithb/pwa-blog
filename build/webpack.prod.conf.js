5d3234d271ce9de2aa7303f0ac10428cdb075707
abdulbasithb <zithron70@gmail.com>
2018-01-02 02:45:04 +0700

    configure service worker

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -106,9 +106,9 @@ const webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'pwa-blog',
       filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      staticFileGlobs: ['docs/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'docs/'
     })
   ]
 })
